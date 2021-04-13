e7f96eb0a6844b258e7f6d7433a38d2041dde6e0
DanilDmitriev1999 <danildmitrievv11@gmail.com>
2021-04-13 20:27:13 +0300

    fix data prepare and data file

--- dssm/preprocessing_data.py
+++ dssm/preprocessing_data.py
@@ -2,44 +2,60 @@ import json
 from typing import List
 from razdel import sentenize
 from pprint import pprint
+from tqdm import tqdm
 
 
-def read_json(filepath: str) -> List[dict]:
-    with open(filepath, "r") as read_file:
-        data = json.load(read_file)
-    return data['data'][0]['paragraphs']
+class PrepareData:
+    def __init__(self, filepath: str,
+                 save_result: bool = True,
+                 path_save: str = '../dssm/data/dssm_train.json') -> None:
+        self.data = self.read_json(filepath)
+        self.dt = self.convert_data(self.data)
 
+        if save_result:
+            self.to_json(self.dt, path_save)
 
-def to_json(json_result: List[dict], path='../dssm/data/dssm_train.json') -> None:
-    with open(path, "w", encoding="utf-8") as file:
-        json.dump(json_result, file, ensure_ascii=False)
+    @staticmethod
+    def convert_data(data: List[dict]) -> List[dict]:
+        prep_data = []
+        for i in tqdm(data, total=len(data)):
+            result_prepare = {'id_wrong_answer': [],
+                              'id_correct_answer': -1,
+                              }
+            split_context = [t.text for t in list(sentenize(i['context']))]
+            word_index = 0
+            question = i['qas'][0]['question']
+            answer = i['qas'][0]['answers'][0]['text']
+            answer_start = i['qas'][0]['answers'][0]['answer_start']
+            result_prepare['question'] = question
+            result_prepare['context'] = split_context
+            result_prepare['short_answer'] = answer
+            # result_prepare['text'] = i
+            for idx, sentences in enumerate(split_context):
+                for _ in sentences:
+                    word_index += 1
+                    if word_index == answer_start:
+                        result_prepare['id_correct_answer'] = idx
+                if idx != result_prepare['id_correct_answer']:
+                    result_prepare['id_wrong_answer'].append(idx)
 
+            prep_data.append(result_prepare)
 
-def prepare_data(data: List[dict]) -> List[dict]:
-    prep_data = []
-    for i in data:
-        result_prepare = {}
-        split_context = [t.text for t in list(sentenize(i['context']))]
-        question = i['qas'][0]['question']
-        answer = i['qas'][0]['answers'][0]['text']
-        result_prepare['question'] = question
-        result_prepare['context'] = split_context
-        result_prepare['short_answer'] = answer
-        for idx, s in enumerate(split_context):
-            if answer in s:
-                result_prepare['answer'] = s
-                result_prepare['id_sentence'] = idx
-                break
+        return prep_data
 
-        prep_data.append(result_prepare)
+    @staticmethod
+    def read_json(filepath: str) -> List[dict]:
+        with open(filepath, "r") as read_file:
+            data = json.load(read_file)
+        return data['data'][0]['paragraphs']
 
-    return prep_data
+    @staticmethod
+    def to_json(json_result: List[dict], path='../dssm/data/dssm_train.json') -> None:
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump(json_result, file, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    train = read_json('../data/sber_squad/train-v1.1.json')
-    dev = read_json('../data/sber_squad/dev-v1.1.json')
-    dssm_train = prepare_data(train)
-    dssm_dev = prepare_data(dev)
-    to_json(dssm_train, '../dssm/data/dssm_train.json')
-    to_json(dssm_dev, '../dssm/data/dssm_dev.json')
+    train = PrepareData('../data/sber_squad/train-v1.1.json', True)
+    dev = PrepareData('../data/sber_squad/dev-v1.1.json', True, '../dssm/data/dssm_dev.json')
+

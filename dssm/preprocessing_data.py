869facb0c48b456714feebaee0315eb27f8d2994
DanilDmitriev1999 <danildmitrievv11@gmail.com>
2021-04-13 21:41:58 +0300

    fix data

--- dssm/preprocessing_data.py
+++ dssm/preprocessing_data.py
@@ -3,6 +3,7 @@ from typing import List
 from razdel import sentenize
 from pprint import pprint
 from tqdm import tqdm
+import collections
 
 
 class PrepareData:
@@ -15,12 +16,13 @@ class PrepareData:
         if save_result:
             self.to_json(self.dt, path_save)
 
-    @staticmethod
-    def convert_data(data: List[dict]) -> List[dict]:
+    # @staticmethod
+    def convert_data(self, data: List[dict]) -> List[dict]:
         prep_data = []
         for i in tqdm(data, total=len(data)):
-            result_prepare = {'id_wrong_answer': [],
-                              'id_correct_answer': -1,
+            result_prepare = {
+                'id_wrong_answer': [],
+                'id_correct_answer': [],
                               }
             split_context = [t.text for t in list(sentenize(i['context']))]
             word_index = 0
@@ -30,14 +32,18 @@ class PrepareData:
             result_prepare['question'] = question
             result_prepare['context'] = split_context
             result_prepare['short_answer'] = answer
-            # result_prepare['text'] = i
+            result_prepare['answer_start'] = answer_start
             for idx, sentences in enumerate(split_context):
                 for _ in sentences:
-                    word_index += 1
                     if word_index == answer_start:
-                        result_prepare['id_correct_answer'] = idx
-                if idx != result_prepare['id_correct_answer']:
+                        result_prepare['id_correct_answer'].append(idx)
+                    word_index += 1
+                # word_index += 1
+                if idx not in result_prepare['id_correct_answer']:
                     result_prepare['id_wrong_answer'].append(idx)
+
+            if not result_prepare['id_correct_answer']:
+                continue
 
             prep_data.append(result_prepare)
 

8ac710bdf5f409bda4548a11c80ab6df2e8390f8
Ryzhtus <shkunkov.a@yandex.ru>
2021-04-28 16:27:30 +0300

    Добавил опцию для вывода Attention

--- model/BERT.py
+++ model/BERT.py
@@ -3,16 +3,18 @@ from transformers import AutoTokenizer, AutoModel
 
 
 class QA2Linear(nn.Module):
-    def __init__(self, transformer_name):
+    def __init__(self, transformer_name, output_attentions):
         super().__init__()
-        self.transformer = AutoModel.from_pretrained(transformer_name, output_hidden_states=True)
+        self.output_attentions = output_attentions
+        self.transformer = AutoModel.from_pretrained(transformer_name, output_hidden_states=True,
+                                                     output_attentions=self.output_attentions)
         self.drop = nn.Dropout(0.1)
         self.fc_start = nn.Linear(768, 1)
         self.fc_end = nn.Linear(768, 1)
 
     def forward(self, ids, mask):
-        transformer_output = self.transformer(ids, attention_mask=mask)['last_hidden_state']
-        transformer_output = self.drop(transformer_output)
+        output = self.transformer(ids, attention_mask=mask)
+        transformer_output = self.drop(output['last_hidden_state'])
         logits_start = self.fc_start(transformer_output)
         logits_end = self.fc_end(transformer_output)
 

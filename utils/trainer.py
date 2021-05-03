e8d28cf6784bdff08c8e01285f3af1f3b3d6479f
Ryzhtus <shkunkov.a@yandex.ru>
2021-05-03 16:24:39 +0300

    Added Exact-Match Metric

--- utils/trainer.py
+++ utils/trainer.py
@@ -2,21 +2,18 @@ import torch
 
 import pytorch_lightning as pl
 from transformers import AdamW
-from torchmetrics import Accuracy, F1
+from utils.metrics import ExactMatch
 
 
 class ModelTrainer(pl.LightningModule):
-    def __init__(self,
-                 model,
-                 criterion,
-                 lr=1e-4):
+    def __init__(self, model, criterion, tokenizer, lr=1e-4):
         super().__init__()
 
         self.model = model
         self.criterion = criterion
 
-        self.acc_start = Accuracy()
-        self.acc_end = Accuracy()
+        self.em = ExactMatch()
+        self.tokenizer = tokenizer
 
         # other
         self.lr = lr
@@ -45,52 +42,50 @@ class ModelTrainer(pl.LightningModule):
 
         start_pred = torch.argmax(logits_start, dim=1).squeeze(-1).cpu().detach().numpy()
         start_gold = start.cpu().detach().numpy()
-        accuracy_start = self.acc_start(start_pred, start_gold)
 
         end_pred = torch.argmax(logits_end, dim=1).squeeze(-1).cpu().detach().numpy()
         end_gold = end.cpu().detach().numpy()
-        accuracy_end = self.acc_end(end_pred, end_gold)
 
-        return loss, loss_start, loss_end, loss_end, accuracy_start, accuracy_end
+        text = text.cpu().detach().numpy()
+
+        for element_idx in range(len(text)):
+            pred = self.tokenizer.decode(text[element_idx][start_pred[element_idx]: end_pred[element_idx]])
+            gold = self.tokenizer.decode(text[element_idx][start_gold[element_idx]: end_gold[element_idx]])
+
+            em_step = self.em(pred, gold)
+
+        return loss, loss_start, loss_end, em_step
 
     def training_step(self, batch, batch_idx):
-        loss, loss_start, loss_end, loss_end, accuracy_start, accuracy_end = self._custom_step(batch)
+        loss, loss_start, loss_end, em = self._custom_step(batch)
 
         values = {'train_loss_sum': loss,
                   'train_start_loss': loss_start,
                   'train_end_loss': loss_end,
-                  'train_accuracy_start': accuracy_start,
-                  'train_accuracy_end': accuracy_end}
+                  'train_exact_match': em}
 
         self.log_dict(values)
 
         return loss
 
     def training_epoch_end(self, outputs):
-        values = {'Epoch Accuracy Start': self.acc_start.compute(),
-                  'Epoch Accuracy End': self.acc_end.compute()}
+        values = {'Train Epoch Exact-Match': self.em.compute()}
 
         self.log_dict(values)
 
     def validation_step(self, batch, batch_idx):
-        loss, loss_start, loss_end, loss_end, accuracy_start, accuracy_end = self._custom_step(batch)
+        loss, loss_start, loss_end, em = self._custom_step(batch)
 
         values = {'val_loss_sum': loss,
                   'val_start_loss': loss_start,
                   'val_end_loss': loss_end,
-                  'val_accuracy_start': accuracy_start,
-                  'val_accuracy_end': accuracy_end}
+                  'val_exact_match': em}
 
         self.log_dict(values)
 
         return loss
 
     def validation_epoch_end(self, outputs):
-        values = {'Epoch Accuracy Start': self.acc_start.compute(),
-                  'Epoch Accuracy End': self.acc_end.compute()}
+        values = {'Validation Epoch Exact-Match': self.em.compute()}
 
         self.log_dict(values)
-
-
-if __name__ == '__main__':
-    pass

7879660988e0c80e88aae367949b6c29a338dc1e
Ryzhtus <shkunkov.a@yandex.ru>
2021-05-09 18:41:16 +0300

    Add f1 score, but it need some improvement

--- utils/trainer.py
+++ utils/trainer.py
@@ -26,6 +26,35 @@ def normalize_answer(s):
     return white_space_fix(remove_articles(remove_punc(lower(s))))
 
 
+class F1Score(Metric):
+    def __init__(self, dist_sync_on_step=False):
+        super().__init__(dist_sync_on_step=dist_sync_on_step)
+
+        self.add_state("score", default=torch.tensor(0))
+        self.add_state("recall", default=torch.tensor(0))
+        self.add_state("precision", default=torch.tensor(0))
+
+    def update(self, pred, gold):
+        pred_tokens = normalize_answer(pred).split()
+        truth_tokens = normalize_answer(gold).split()
+
+        # if either the prediction or the truth is no-answer then f1 = 1 if they agree, 0 otherwise
+        if len(pred_tokens) == 0 or len(truth_tokens) == 0:
+            return int(pred_tokens == truth_tokens)
+
+        common_tokens = set(pred_tokens) & set(truth_tokens)
+
+        # if there are no common tokens then f1 = 0
+        if len(common_tokens) == 0:
+            return 0
+
+        self.precision = torch.tensor(len(common_tokens) / len(pred_tokens))
+        self.recall = torch.tensor(len(common_tokens) / len(truth_tokens))
+
+    def compute(self):
+        return 2 * (self.precision * self.recall) / (self.precision + self.recall)
+
+
 class ExactMatch(Metric):
     def __init__(self, dist_sync_on_step=False):
         super().__init__(dist_sync_on_step=dist_sync_on_step)
@@ -54,6 +83,7 @@ class ModelTrainer(pl.LightningModule):
         self.criterion = criterion
 
         self.em = ExactMatch()
+        self.f1 = F1Score()
         self.tokenizer = tokenizer
 
         # other
@@ -94,39 +124,44 @@ class ModelTrainer(pl.LightningModule):
             gold = self.tokenizer.decode(text[element_idx][start_gold[element_idx]: end_gold[element_idx]])
 
             em_step = self.em(pred, gold)
+            f1_step = self.f1(pred, gold)
 
-        return loss, loss_start, loss_end, em_step
+        return loss, loss_start, loss_end, em_step, f1_step
 
     def training_step(self, batch, batch_idx):
-        loss, loss_start, loss_end, em = self._custom_step(batch)
+        loss, loss_start, loss_end, em, f1 = self._custom_step(batch)
 
         values = {'train_loss_sum': loss,
                   'train_start_loss': loss_start,
                   'train_end_loss': loss_end,
-                  'train_exact_match': em}
+                  'train_exact_match': em,
+                  'train_f1': f1}
 
         self.log_dict(values)
 
         return loss
 
     def training_epoch_end(self, outputs):
-        values = {'Train Epoch Exact-Match': self.em.compute()}
+        values = {'Train Epoch Exact-Match': self.em.compute(),
+                  'Train Epoch F1': self.f1.compute()}
 
         self.log_dict(values)
 
     def validation_step(self, batch, batch_idx):
-        loss, loss_start, loss_end, em = self._custom_step(batch)
+        loss, loss_start, loss_end, em, f1 = self._custom_step(batch)
 
         values = {'val_loss_sum': loss,
                   'val_start_loss': loss_start,
                   'val_end_loss': loss_end,
-                  'val_exact_match': em}
+                  'val_exact_match': em,
+                  'val_f1': f1}
 
         self.log_dict(values)
 
         return loss
 
     def validation_epoch_end(self, outputs):
-        values = {'Validation Epoch Exact-Match': self.em.compute()}
+        values = {'Validation Epoch Exact-Match': self.em.compute(),
+                  'Validation Epoch F1': self.f1.compute()}
 
         self.log_dict(values)
